ad53854b044685697571e37fc31c55fdd020f604
yrohan780 <rohan@triosoft.in>
2024-07-01 14:59:25 +0530

    DFGDFGDFG

--- src/Components/BrowseCity.js
+++ src/Components/BrowseCity.js
@@ -55,8 +55,8 @@ const BrowseCity = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
+          slidesToShow: 5,
+          slidesToScroll: 5,
           infinite: true,
           dots: true,
         },
@@ -64,16 +64,16 @@ const BrowseCity = () => {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
+          slidesToShow: 3,
+          slidesToScroll: 3,
           initialSlide: 2,
         },
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToShow: 3,
+          slidesToScroll: 3,
         },
       },
     ],
